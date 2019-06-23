575d70a9dd8fd7d5b43b4a714c4f952bae9eed99
ohelly <rpashtet0815@gmail.com>
2019-06-23 21:04:22 +0300

    add testmaps

--- includes/fillit.h
+++ includes/fillit.h
@@ -20,16 +20,16 @@
 # include "libft.h"
 
 char	***ft_split(char ***arr, char *buf, char c);
+char	***ft_moveblock(char ***block);
+char	**ft_newsqr(char **min, int len);
 int		ft_checkvalid(char **block);
 int		ft_minsquare(char ***block);
-char	***ft_moveblock(char ***block);
 int		ft_solvesquare(char ***block, char **min, int z, int bc);
 int		ft_solvesquare2(char ***block, char **min, int z, int bc);
-char	**ft_newsqr(char **min, int len);
 int		ft_counter(void);
-void	ft_output(char **min, char ***block);
-void	ft_del(char **min);
 int		ft_lastletter(char ***block, char **min);
 int		ft_start(char ***block, char **min);
+void	ft_output(char **min, char ***block);
+void	ft_del(char **min);
 
 #endif
